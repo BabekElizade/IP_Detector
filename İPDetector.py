54c505145bf482b358ffe1b941dad5acebcde726
Babek Elizade <141303799+BabekElizade@users.noreply.github.com>
2025-11-01 19:51:16 +0400

    Add files via upload

---
+++ İPDetector.py
@@ -0,0 +1,14 @@
+import requests
+while True:
+    İP_input = input("IP unvanini daxil et: ")
+    if(İP_input == "C" or İP_input == "c"):
+        break
+    url = f"https://ipinfo.io/{İP_input}/json"
+    response = requests.get(url)
+    data = response.json()
+
+    print("\nMəlumat:")
+    print("IP:", data.get("ip"))
+    print("Ölkə:", data.get("country"))
+    print("Şəhər:", data.get("city"))
+    print("Provayder:", data.get("org"))
